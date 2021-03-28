c8ef5e047f9eb0920ed464d0138afc697b581240
hazamaswag <hamzasayyid@gmail.com>
2021-03-28 10:42:31 -0700

    Refactor tests and fix purge

--- enzyme.ts
+++
@@ -1,9 +0,0 @@
-// const Adapter = require("@wojtekmaj/enzyme-adapter-react-17");
-
-// require("enzyme").configure({ adapter: new Adapter() });
-
-import Enzyme from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-
-// Configure Enzyme with React 16 adapter
-Enzyme.configure({ adapter: new Adapter() });
--- jest.config.js
+++ jest.config.js
@@ -4,19 +4,18 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
-  testMatch: ["**/*.(test|spec).(ts|tsx)"],
+  testMatch: ["**/__tests__/*.(ts|tsx)"],
   globals: {
     "ts-jest": {
       babelConfig: false,
-      tsconfig: "jest.tsconfig.json",
+      tsconfig: "tsconfig.jest.json",
     },
   },
   coveragePathIgnorePatterns: ["/node_modules/", "enzyme.js"],
-  setupFilesAfterEnv: ["<rootDir>/enzyme.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   coverageReporters: ["json", "lcov", "text", "text-summary"],
+  testPathIgnorePatterns: ["./.next/", "./node_modules/"],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/mocks.js",
-    "\\.(css|less|scss)$": "identity-obj-proxy",
+    "\\.(css|less|scss)$": "<rootDir>/__mocks__/styleMock.js",
   },
 };
