d50b66e03fece88a98df04f4ffc182d3fb0706aa
hazamaswag <hamzasayyid@gmail.com>
2021-07-12 00:53:31 -0700

    Added colors for vowels in arabic

---
+++ src/components/ParseColor.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const ParseColor = ({ vowel }) => {
+  console.log(vowel);
+  let txt = "";
+  if (!vowel) {
+    txt = "yeet";
+  } else {
+    txt = "no yeet";
+  }
+  console.log(txt);
+  return (
+    <div>
+      {vowel ? (
+        <span>ه ه ه</span>
+      ) : (
+        <p>
+          ه
+          <span lang={"ara"} style={{ color: "blue" }}>
+            َ &nbsp;
+          </span>
+          ه
+          <span lang={"ara"} style={{ color: "blue" }}>
+            ِ &nbsp;
+          </span>
+          ه
+          <span lang={"ara"} style={{ color: "blue" }}>
+            ُ &nbsp;
+          </span>
+        </p>
+      )}
+      {/* {console.log(txt)} */}
+      {/* {txt} */}
+    </div>
+  );
+};
--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,10 +1,13 @@
+import { spawn } from "node:child_process";
 import React from "react";
 import { useState } from "react";
 import { IContent } from "../../IChapters";
+import { ParseColor } from "./ParseColor";
 
 const Toggle = ({ content }: IContent) => {
   const [text, setText] = useState(content.unvoweled);
-  const [vowel, setVowel] = useState("هِ هَ هُ");
+  // const [vowel, setVowel] = useState("هِ هَ هُ");
+  const [vowel, setVowel] = useState(false);
   if (!text) return null;
 
   return (
@@ -17,14 +20,17 @@ const Toggle = ({ content }: IContent) => {
           onClick={() => {
             if (text === content.unvoweled) {
               setText(content.voweled);
-              setVowel("ه ه ه");
+              // setVowel("ه ه ه");
+              setVowel(true);
             } else {
               setText(content.unvoweled);
-              setVowel("هِ هَ هُ");
+              // setVowel("هِ هَ هُ");
+              setVowel(false);
             }
           }}
         >
-          {vowel}
+          <ParseColor vowel={vowel} />
+          {/* {console.log(vowel)} */}
         </button>
       </div>
       <p className="text-left ">{content.english}</p>
